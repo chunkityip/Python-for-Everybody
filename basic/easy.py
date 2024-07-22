af4a8eb836c19291df5b3ae63fdf319cf176c879
Chunkit Yip <chunkit.yip@peopleshores.com>
2024-07-22 12:24:07 -0400

    to be continue

---
+++ basic/easy.py
@@ -0,0 +1,45 @@
+# 1. Hello, World!: Print "Hello , World!" to the console
+# print("Hello , World!")
+
+"""
+2. Sum of Two Numbers: Write a program that takes a numbe as input and 
+"""
+# num1 = int(input("Enter the number one: "))
+# num2 = int(input("Enter the number one: "))
+# print(num1 + num2)
+
+"""
+3. 
+Write a program that takes a number as input and prints 
+"Even" if the number is even, and "Odd" if the number is odd.
+"""
+def checkNumber(num):
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")    
+
+num = int(input("Enter the number: "))
+checkNumber(num)
+
+
+"""
+
+4. Largest of Three Numbers
+Write a program that takes three numbers as input and 
+prints the largest number
+
+"""
+# num1 = int(input("Enter one number: "))
+# num2 = int(input("Enter two number: "))
+# num3 = int(input("Enter three number: "))
+
+# largest = max(num1 , num2 , num3)
+# print(largest)
+
+
+
+
+
+
+
